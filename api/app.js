5341ab6036df74854ab45f0f20264b6ee89c5c4e
TheDrizzyWay <43966210+TheDrizzyWay@users.noreply.github.com>
2018-10-19 22:59:52 +0100

    adjusted app.js

--- api/app.js
+++ api/app.js
@@ -14,7 +14,13 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 3000;
-// if (!module.parent) for test watch
-app.listen(port, () => console.log(`Server running on port ${port}`));
+/* for testing
+if (!module.parent) {
+	app.listen(port, () => console.log(`Server running on port ${port}`));
+}
+*/
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(process.env.PORT || 5000, () => console.log('Server running on localhost:5000'));
+}
 
 export default app;
