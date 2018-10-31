0f2b13e740780cdba8a0f75d478f9fd9606816a1
TheDrizzyWay <43966210+TheDrizzyWay@users.noreply.github.com>
2018-10-31 23:44:18 +0100

    Fixed errors in sql queries

--- api/v1/database/models/sales.js
+++ api/v1/database/models/sales.js
@@ -1,12 +1,12 @@
 export default {
 	CREATE_TABLE: `CREATE TABLE IF NOT EXISTS sales
 	(
-	id SERIAL PRIMARY KEY,
-  	productName SERIAL REFERENCES products(name) ON DELETE CASCADE,
-  	price integer REFERENCES products(price) ON DELETE CASCADE,
+	  id SERIAL PRIMARY KEY,
+  	productName varchar(100) NOT NULL,
+  	price integer NOT NULL,
   	quantitySold integer NOT NULL,
   	total integer NOT NULL,
-  	sellerId SERIAL REFERENCES users(id) ON DELETE CASCADE,
+  	sellerId integer NOT NULL,
   	created_at TIMESTAMP DEFAULT NOW()
 	)`,
 };
